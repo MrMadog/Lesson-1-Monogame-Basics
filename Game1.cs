a59423f00da133fb55d10b4d166f8ea66771e21b
Maddox Macnamara <tpmac001maddog@gmail.ca>
2023-04-04 21:10:50 -0400

    just starting

--- Game1.cs
+++ Game1.cs
@@ -6,6 +6,8 @@ namespace Monogame_Basics
 {
     public class Game1 : Game
     {
+        Texture2D ballTexture;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -19,15 +21,19 @@ namespace Monogame_Basics
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            _graphics.PreferredBackBufferWidth = 1280; // Sets the width of the window
+            _graphics.PreferredBackBufferHeight = 720; // Sets the height of the window
+            _graphics.ApplyChanges(); // Applies the new dimensions
+            this.Window.Title = "My First Monogame Project";
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
             // TODO: use this.Content to load your game content here
+            ballTexture = Content.Load<Texture2D>("ball");
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -42,9 +48,12 @@ namespace Monogame_Basics
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.LightGray);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(ballTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
